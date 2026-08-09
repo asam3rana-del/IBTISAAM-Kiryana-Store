f63b2123978858fe5035f7a6f2951e4e74dd5f8b
asam3rana-del <asam3rana@gmail.com>
2026-08-09 18:34:37 +0500

    Update Database.kt

--- app/src/main/java/com/grocerypos/v11/Database.kt
+++ app/src/main/java/com/grocerypos/v11/Database.kt
@@ -194,7 +194,7 @@ interface ProductDao {
     entities=[Product::class,Customer::class,Supplier::class,Sale::class,SaleItem::class,
         Payment::class,Purchase::class,PurchaseItem::class,ReturnLine::class,User::class,Audit::class,
         Expense::class,HeldBill::class],
-    version=5, exportSchema=false
+    version=6, exportSchema=false
 )
 abstract class PosDatabase:RoomDatabase(){
     abstract fun productDao():ProductDao
