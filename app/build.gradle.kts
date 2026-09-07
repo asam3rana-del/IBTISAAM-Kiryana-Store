757b52311d476b061b9537632a46efc2180c83b9
asam3rana-del <asam3rana@gmail.com>
2026-09-07 16:56:35 +0500

    Add RecyclerView dependency to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,6 +88,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.activity:activity-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
